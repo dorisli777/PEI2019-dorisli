e41028afb7f3c05a0ab2086ed6683062a05b3edc
dorisli777 <47066062+dorisli777@users.noreply.github.com>
2019-06-10 16:52:18 -0400

    Add files via upload
    First script created; test code

---
+++ scripto.sh
@@ -0,0 +1,12 @@
+#! /bin/sh/
+#
+# This function prints an input name 
+#
+# Last modified by dorisli@ 10-June-2019
+
+inputname=$1
+echo My name is : $inputname
+
+echo Tests
+echo Using double parens : "$1"
+echo Using single parens : '$1'
