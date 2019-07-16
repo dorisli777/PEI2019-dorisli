94d30d6287d8a0c446ebeebefba0ecb33a6f62ca
Doris Li <dorisli@ariel.princeton.edu>
2019-07-16 16:41:44 -0400

    Update

--- tauptimeP.sh
+++ tauptimeP.sh
@@ -3,7 +3,7 @@
 # tauptime depth distance 
 # where depth is in km and distance in degree
 #
-# Caculates first P-wave arrival time in seconds
+# Calculates first P-wave arrival time in seconds
 
 taup_time -ph P -h $1 -deg $2 | awk '/ P / {print $4}' | awk 'NR ==1 {print $1}'
 
